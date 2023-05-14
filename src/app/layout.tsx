43c2b97504dbae601b558655bf273ca1b4db1b3a
Darken141 <Darken141@gmail.com>
2023-05-14 23:11:39 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,38 @@
+import {Metadata} from 'next'
+
 import '@/app/globals.css'
 import Header from '@/app/components/header/header.component'
 import Footer from '@/app/components/footer/footer.component'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'ParisDot Conf W-shops',
   description:
     '3 days of conference and workshops, organized by leading Polkadot projects for the broader crypto family. 6/19 - 21 in Paris, France',
+    icons: [
+      {
+        rel:"apple-touch-icon",
+        sizes:"180x180",
+        url:"/favicon/apple-touch-icon.png",
+        type:"image/png"
+
+      },
+      {
+        rel:"icon",
+        sizes:"32x32",
+        url:"/favicon/favicon-32x32.png",
+        type:"image/png"
+      },
+      {
+        rel:"icon",
+        sizes:"16x16",
+        url:"/favicon/favicon-16x16.png",
+        type:"image/png"
+
+      },
+    ],
+    manifest: "/favicon/site.webmanifest",
+    themeColor: "#fff",
+    
 }
 
 export default function RootLayout({
