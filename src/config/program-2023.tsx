6a3ebece68ca1fc0593e3cd6597c74c3fce82fb1
IvetaR <138973706+IvetaR@users.noreply.github.com>
2023-07-08 12:30:19 +0200

    update day1 program

--- src/config/program-2023.tsx
+++ src/config/program-2023.tsx
@@ -10,25 +10,180 @@ export const day1Program = [
   {
     heading: 'program main stage',
     id: 'day-1-1',
+    title: 'Day 1 - 18th July - Understanding and Fostering Blockchain Adoption',
+    showContent: false,
+    items: [
+      {
+        id: 'item-1',
+        time: '11:00 - 11:20',
+        title: 'Opening Keynote',
+        speakers: [],
+      },
+      {
+        type: 'Panel',
+        id: 'item-2',
+        time: '11:20 - 12:05',
+        title: 'The Emerging Web3: How Blockchain is Transforming the Internet',
+      },
+      {
+        type: 'Workshop',
+        id: 'item-3',
+        time: '12:05 - 12:30',
+        title: 'Fostering Mass Adoption of Blockchain',
+        speakers: [
+          {
+            organization: {
+              name: 'FFPB',
+              logo: {
+                src: Ffpb,
+                alt: 'FFPB',
+              },
+            },
+            image: {
+              src: Remy,
+              alt: 'Remy Ozcan',
+            },
+            name: 'Remy Ozcan',
+            title: 'President',
+            socials: {
+              linkedin: 'https://www.linkedin.com/in/r%C3%A9my-ozcan/',
+            },
+            bio: '',
+          },
+          {
+            organization: {
+              name: 'Barreau de Paris',
+              logo: {
+                src: Barreau,
+                alt: 'Barreau de Paris',
+              },
+            },
+            image: {
+              src: Sabine,
+              alt: 'Sabine Van Haecke Lepic',
+            },
+            name: 'Sabine Van Haecke Lepic',
+            title: 'Lecturer, Researcher',
+            socials: {
+              linkedin:
+                'https://www.linkedin.com/in/sabine-van-haecke-lepic-ph-d-37b735140/',
+            },
+            bio: '',
+          },
+        ],
+      },
+      {
+        type: 'Panel',
+        id: 'item-4',
+        time: '12:30 - 13:20',
+        title: 'Bridging the Gap: How to Drive Blockchain Adoption Among the General Public',
+        speakers: [
+          {
+            organization: {
+              name: 'FFPB',
+              logo: {
+                src: Ffpb,
+                alt: 'FFPB',
+              },
+            },
+            image: {
+              src: Remy,
+              alt: 'Remy Ozcan',
+            },
+            name: 'Remy Ozcan',
+            title: 'President',
+            socials: {
+              linkedin: 'https://www.linkedin.com/in/r%C3%A9my-ozcan/',
+            },
+            bio: '',
+          },
+          {
+            organization: {
+              name: 'Barreau de Paris',
+              logo: {
+                src: Barreau,
+                alt: 'Barreau de Paris',
+              },
+            },
+            image: {
+              src: Sabine,
+              alt: 'Sabine Van Haecke Lepic',
+            },
+            name: 'Sabine Van Haecke Lepic',
+            title: 'Lecturer, Researcher',
+            socials: {
+              linkedin:
+                'https://www.linkedin.com/in/sabine-van-haecke-lepic-ph-d-37b735140/',
+            },
+            bio: '',
+          },
+        ],
+      },
+      {
+        id: 'item-5',
+        time: '13:20 - 14:10',
+        title: 'LUNCH',
+      },
+      {
+        id: 'item-6',
+        time: '14:10 - 14:30',
+        title: 'FFPB initiatives with institution and corporate',
+      },
+      {
+        type: 'Panel',
+        id: 'item-7',
+        time: '14:30 - 15:15',
+        title: 'DAO : new paradigm for modern business approach',
+      },
+      {
+        id: 'item-8',
+        time: '15:15 - 15:30',
+        title: '"How to avoid chaos” a story between Humans & Blockchain',
+      },
+      {
+        type: 'Panel',
+        id: 'item-9',
+        time: '15:30 - 16:20',
+        title: '"How to avoid Chaos" the theory part',
+      },
+      {
+        id: 'item-10',
+        time: '16:20 - 16:40',
+        title: 'BREAK',
+      },
+      {
+        type: 'Panel',
+        id: 'item-11',
+        time: '16:40 - 17:30',
+        title: '"How to avoid Chaos" use-cases part',
+      },
+      {
+        id: 'item-12',
+        time: '17:30 - 18:00',
+        title: 'Closing Keynote',
+      },
+    ],
+  },
+  {
+    heading: 'polkadot stage',
+    id: 'day-1-2',
     title: 'program for 18.07',
     showContent: false,
     items: [
       {
         id: 'item-1',
-        time: '12:00 - 14:00',
-        title: 'Registration - pillar room',
+        time: '11:30 - 11:45',
+        title: 'Privacy in DeFi with ZK',
       },
       {
-        type: 'Panel',
         id: 'item-2',
-        time: '14:00 - 14:15',
-        title: 'Opening the conference Hello from the organizers',
+        time: '11:45 - 12:00',
+        title: 'DePin.. bridging the gap between the real world and crypto',
       },
       {
-        type: 'Workshop',
         id: 'item-3',
-        time: '14:15 - 14:45',
-        title: 'Blockchains That Evolve with Your Business',
+        time: '12:00 - 12:15',
+        title: 'Protecting your tokens with Logion',
         speakers: [
           {
             organization: {
@@ -73,123 +228,73 @@ export const day1Program = [
       },
       {
         id: 'item-4',
-        time: '14:45 - 15:15',
-        title: 'Mojo over Ego: decentralizing leadership',
-        speakers: [
-          {
-            organization: {
-              name: 'FFPB',
-              logo: {
-                src: Ffpb,
-                alt: 'FFPB',
-              },
-            },
-            image: {
-              src: Remy,
-              alt: 'Remy Ozcan',
-            },
-            name: 'Remy Ozcan',
-            title: 'President',
-            socials: {
-              linkedin: 'https://www.linkedin.com/in/r%C3%A9my-ozcan/',
-            },
-            bio: '',
-          },
-          {
-            organization: {
-              name: 'Barreau de Paris',
-              logo: {
-                src: Barreau,
-                alt: 'Barreau de Paris',
-              },
-            },
-            image: {
-              src: Sabine,
-              alt: 'Sabine Van Haecke Lepic',
-            },
-            name: 'Sabine Van Haecke Lepic',
-            title: 'Lecturer, Researcher',
-            socials: {
-              linkedin:
-                'https://www.linkedin.com/in/sabine-van-haecke-lepic-ph-d-37b735140/',
-            },
-            bio: '',
-          },
-        ],
+        time: '12:15 - 12:30',
+        title: 'Light Clients: On the Critical Path to Decentralisation',
+      },
+      {
+        id: 'item-5',
+        time: '12:30 - 12:45',
+        title: 'Safe, fun and efficient: smart contracts in ink!.',
+      },
+      {
+        id: 'item-6',
+        time: '12:45 - 13:00',
+        title: 'Data Ownership with Decentralized Storage',
+      },
+      {
+        id: 'item-7',
+        time: '13:00 - 14:00',
+        title: 'LUNCH',
       },
       {
         id: 'item-8',
-        time: '18:30 - 18:35',
-        title: 'Closing',
-      },
-    ],
-  },
-  {
-    heading: 'polkadot stage',
-    id: 'day-1-2',
-    title: 'program for 18.07',
-    showContent: false,
-    items: [
-      {
-        id: 'item-1',
-        time: '12:00 - 14:00',
-        title: 'Registration - pillar room',
-      },
-      {
-        id: 'item-2',
         time: '14:00 - 14:15',
-        title: 'Opening the conference Hello from the organizers',
+        title: 'Remote Execution Using XCM',
       },
       {
-        id: 'item-3',
-        time: '14:15 - 14:45',
-        title: 'Blockchains That Evolve with Your Business',
-        speakers: [
-          {
-            organization: {
-              name: 'FFPB',
-              logo: {
-                src: Ffpb,
-                alt: 'FFPB',
-              },
-            },
-            image: {
-              src: Remy,
-              alt: 'Remy Ozcan',
-            },
-            name: 'Remy Ozcan',
-            title: 'President',
-            socials: {
-              linkedin: 'https://www.linkedin.com/in/r%C3%A9my-ozcan/',
-            },
-            bio: '',
-          },
-          {
-            organization: {
-              name: 'Barreau de Paris',
-              logo: {
-                src: Barreau,
-                alt: 'Barreau de Paris',
-              },
-            },
-            image: {
-              src: Sabine,
-              alt: 'Sabine Van Haecke Lepic',
-            },
-            name: 'Sabine Van Haecke Lepic',
-            title: 'Lecturer, Researcher',
-            socials: {
-              linkedin:
-                'https://www.linkedin.com/in/sabine-van-haecke-lepic-ph-d-37b735140/',
-            },
-            bio: '',
-          },
-        ],
+        id: 'item-9',
+        time: '14:15 - 14:30',
+        title: "How NFTs can change your user's entire experience",
       },
       {
-        id: 'item-8',
-        time: '18:30 - 18:35',
-        title: 'Closing',
+        id: 'item-10',
+        time: '14:30 - 14:45',
+        title: 'Phat Contract. Smart Contracts. Now Smarter',
+      },
+      {
+        id: 'item-11',
+        time: '14:45 - 15:00',
+        title: 'TBD',
+      },
+      {
+        id: 'item-12',
+        time: '15:00 - 15:15',
+        title: 'Developing better, faster cross-chain dApps with data indexing',
+      },
+      {
+        id: 'item-13',
+        time: '15:15 - 15:30',
+        title: 'Building Smarter Solutions with Vara',
+      },
+      {
+        id: 'item-14',
+        time: '15:30 - 15:45',
+        title: 'The interoperable future: How SORA Card brings tradfi to substrate DeFi',
+      },
+      {
+        id: 'item-15',
+        time: '15:45 - 16:00',
+        title: 'Real World Assets and Token Economy on Polkadot',
+      },
+      {
+        id: 'item-16',
+        time: '16:00 - 16:15',
+        title: 'Decentralized Storage Infrastructure / Scalability',
+      },
+      {
+        id: 'item-17',
+        time: '16:15 - 16:30',
+        title: 'TBD',
       },
     ],
   },
@@ -200,19 +305,10 @@ export const day1Program = [
     showContent: false,
     items: [
       {
+        type: 'Networking session'
         id: 'item-1',
-        time: '12:00 - 14:00',
-        title: 'Registration - pillar room',
-      },
-      {
-        id: 'item-2',
-        time: '14:00 - 14:15',
-        title: 'Opening the conference Hello from the organizers',
-      },
-      {
-        id: 'item-8',
-        time: '18:30 - 18:35',
-        title: 'Closing',
+        time: '18:00 - 20:30',
+        title: 'Food and Drinks"',
       },
     ],
   },
