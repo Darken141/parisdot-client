933a8a6b611298ebcbaf6b5f24c5021a942428ef
Darken141 <Darken141@gmail.com>
2023-07-09 13:18:50 +0200

    add arrow to speaker, adjust styles

--- src/components/speaker-overview/speaker-overview.component.tsx
+++ src/components/speaker-overview/speaker-overview.component.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { IoLogoTwitter } from 'react-icons/io'
 import { AiFillLinkedin } from 'react-icons/ai'
 import { FaUserAlt } from 'react-icons/fa'
+import { FiArrowDownCircle, FiArrowUpCircle } from 'react-icons/fi'
 
 import { ISpeaker } from '@/config/speakers'
 
@@ -21,7 +22,9 @@ export default function SpeakerOverview({ speaker }: IProps) {
       onClick={() => speaker.bio && setShowContent(!showContent)}
       className={`grid grid-cols-1 sm:grid-cols-[11rem,1fr] gap-[1.5rem] p-[1.5rem] rounded-[2rem] transition-all ${
         speaker.bio ? 'hover:bg-[#082240] cursor-pointer' : ''
-      }`}
+      }
+        ${showContent ? 'bg-[#082240]' : ''}
+      `}
     >
       {speaker.image ? (
         <figure className="w-[11rem] h-[11rem] rounded-[50%] overflow-hidden mb-[1.26rem] mt-[1rem]">
@@ -38,43 +41,55 @@ export default function SpeakerOverview({ speaker }: IProps) {
         </figure>
       )}
       <div className="">
-        <div className="flex gap-[1.5rem]">
-          <div>
-            <h3 className="font-bold text-[2rem]">{speaker.name}</h3>
-            {speaker.organization.link ? (
+        <div className="flex justify-between gap-[2rem]">
+          <div className="">
+            <div>
+              <h3 className="font-bold text-[2rem]">{speaker.name}</h3>
+              {speaker.organization.link ? (
+                <a
+                  href={speaker.organization.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-all text-[#ff008c]"
+                >
+                  <p className="text-[1.6rem]">{speaker?.organization.name}</p>
+                </a>
+              ) : (
+                <p className="text-[1.6rem] ">{speaker?.organization.name}</p>
+              )}
+            </div>
+          </div>
+          <div className="flex gap-[1.5rem] items-start">
+            {speaker.socials && speaker.socials.twitter && (
               <a
-                href={speaker.organization.link}
+                href={speaker.socials.twitter}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="transition-all text-[#ff008c]"
+                className="text-[3rem] text-[#ff008c]"
               >
-                <p className="text-[1.6rem]">{speaker?.organization.name}</p>
+                <IoLogoTwitter />
               </a>
-            ) : (
-              <p className="text-[1.6rem] ">{speaker?.organization.name}</p>
+            )}
+            {speaker.socials && speaker.socials.linkedin && (
+              <a
+                href={speaker.socials.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[3rem] text-[#ff008c]"
+              >
+                <AiFillLinkedin />
+              </a>
+            )}
+            {speaker.bio && (
+              <figure
+                className={`flex items-center justify-center text-[2rem] p-[0.6rem] rounded-[50%] ${
+                  showContent ? 'bg-[#FF008C]' : 'bg-[#0C2746]'
+                }`}
+              >
+                {showContent ? <FiArrowUpCircle /> : <FiArrowDownCircle />}
+              </figure>
             )}
           </div>
-
-          {speaker.socials && speaker.socials.twitter && (
-            <a
-              href={speaker.socials.twitter}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-[3rem] text-[#ff008c]"
-            >
-              <IoLogoTwitter />
-            </a>
-          )}
-          {speaker.socials && speaker.socials.linkedin && (
-            <a
-              href={speaker.socials.linkedin}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-[3rem] text-[#ff008c]"
-            >
-              <AiFillLinkedin />
-            </a>
-          )}
         </div>
         <AnimatePresence>
           {showContent && (
